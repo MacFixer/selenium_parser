d5e2571bcce0b7196e6e05ae3e92064efb4d2cde
MacFixer <den.a.borisenko@gmail.com>
2020-05-08 01:18:45 +0300

    Automatic click on each button for showing phone numbers

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import unittest
 import time
+import random
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
@@ -13,29 +14,38 @@ class AvitoParser:
     def setUp(self):
         self.driver =webdriver.Chrome()
         driver = self.driver
+        driver.implicitly_wait(8)
         driver.get("https://www.avito.ru/krasnodar/predlozheniya_uslug/krasota_zdorove-ASgBAgICAUSYC6qfAQ?q=%D0%BD%D0%B0%D1%80%D0%B0%D1%89%D0%B8%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5+%D1%80%D0%B5%D1%81%D0%BD%D0%B8%D1%86")
 
         try:
             elems = driver.find_elements_by_class_name("item__line")
             # elems = driver.find_elements_by_class_name("item_table-wrapper")
 
-            wait = WebDriverWait(driver, 20)
+            wait = WebDriverWait(driver, 60)
 
             for elem in elems:
 
                 title_elem = elem.find_element_by_class_name("snippet-title")
                 title_text = title_elem.text
                 hover = ActionChains(driver).move_to_element(elem)
-                hover.perform()
-
                 price_str = elem.find_element_by_class_name("snippet-price-row").text
-                phone_button = elem.find_element_by_class_name("js-item_table-extended-contacts")
-                wait.until(EC.visibility_of_element_located((By.TAG_NAME, "button"))).click()
+                district = elem.find_element_by_class_name("item-address-georeferences-item__content").text
+                hover.perform()
+                phone_button = elem.find_element_by_class_name("js-item-extended-contacts").click()
+
+                rand_sleep = random.randint(15, 27)
+                print(rand_sleep/10)
+                time.sleep(rand_sleep/10)
+
+                # wait.until(EC.visibility_of_element_located((By.TAG_NAME, "button"))).click()
 
                 # tel_img = elem.find_element_by_class_name("item-extended-phone")
 
+                # tel_img = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "item-extended-phone"))).get_attribute('src')
+
                 # tel_image = elem
                 print(title_text, end="")
+                print(district, end="")
                 # print(tel_img, end="")
                 print(price_str, end="\n")
                 # time.sleep(1)
